09efff1fdf59819f1c522cf3a914c8e8fb1c6a05
akshitvilager <kumakshit15@gmail.com>
2022-08-20 19:14:53 +0530

    navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,24 +9,21 @@ import { Transition } from "@headlessui/react";
 const Navbar = (props) => {
   const router = useRouter();
   const [isOpen, setIsOpen] = useState(false);
-
   return (
     <>
       <nav
         id={extra.edu}
-        className="hidden md:flex justify-between w-full mx-auto md:flex-row md:  bg-[#fff] md:z-[1] shadow-[#EEEEEE]"
+        className="flex justify-between w-full mx-auto md:flex-row md:  bg-[#fff] md:z-[1] shadow-[#EEEEEE]"
       >
         <Link href="/" passHref>
           <div id={extra.edu} className="mt-2 cursor-pointer">
             <span className="text-4xl lin font-bold p-2 tracking-wide">
               donate
-              <span className="text-[#00E676] text-4xl py-[-20px]">
-                hut
-              </span>
+              <span className="text-[#00E676] text-4xl py-[-20px]">hut</span>
             </span>
           </div>
         </Link>
-        <div className="flex text-lg font-[600] tracking-wide px-2 pt-2">
+        {/* <div className="hidden md:flex text-lg font-[600] tracking-wide px-2 pt-2">
           <span className="p-4">
             <Link href="/">
               <a className={router.pathname == "/" ? "active" : ""}>Home</a>
@@ -60,14 +57,10 @@ const Navbar = (props) => {
               </a>
             </Link>
           </span>
-        </div>
+        </div> */}
         <div className="flex p-4">
-          <div className="p-1">
-            <input
-              className="outline-0 w-28 rounded-3xl border-2 px-6 border-[#00E676] text-[#00E676]"
-              type="text"
-            />
-            <FaSearch className="mt-[-24px] px-1 text-xl text-[#00E676]" />
+          <div className="py-1 px-2">
+            <FaSearch className="text-xl text-[#00E676]" />
           </div>
           <h1 className="px-2 py-1 font-semibold">Akshit Kumar</h1>
           <div className="px-2">
