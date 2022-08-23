22fe4da255ec9db4348e8286d01f4aec11f20c53
akshitvilager <kumakshit15@gmail.com>
2022-08-23 14:20:38 +0530

    fd

--- components/Navbar1.js
+++ components/Navbar1.js
@@ -32,7 +32,7 @@ import { useState } from 'react';
             xmlns='http://www.w3.org/2000/svg'
           >
             <path
-              strokeLinecap='round'
+              strokeLinecap='rond'
               strokeLinejoin='round'
               strokeWidth={2}
               d='M4 6h16M4 12h16M4 18h16'
